b8a2e51faec7d713d3298da9b44dde5254ce2dbd
smchammer3-cyber <245281254+smchammer3-cyber@users.noreply.github.com>
2025-12-02 18:17:49 -0600

    6A-1

---
+++ src/ui/AppShell.tsx
@@ -0,0 +1,14 @@
+// JARVIS_CHANGE
+// Date: 2025-12-02
+// Step: 6A-0 - Create AppShell directory + placeholder file.
+// This file will be filled in during steps 6A-1 through 6A-5.
+
+import React from 'react'
+
+export function AppShell() {
+  return (
+    <div>
+      {/* AppShell layout will be implemented in 6A-1 and onward */}
+    </div>
+  )
+}
