08cd5a80cc34a3af68d24be72317e8eae8c6090a
Marcus Couture <smchammer3@gmail.com>
2026-07-21 01:46:14 -0500

    Apply reviewed premise declaration-strength policy

--- src/core/causalGeology/index.ts
+++ src/core/causalGeology/index.ts
@@ -19,6 +19,7 @@ export { resolvePlanetInitialConditionBundle } from './initialConditionAuthority
 export * from './premiseResearchContracts';
 export * from './premiseResearchFirewall';
 export * from './premiseResolver';
+export { resolvePlanetaryPremise } from './premiseResolverPolicy';
 export * from './stageResult';
 export * from './spatial';
 export * from './researchLedger';
